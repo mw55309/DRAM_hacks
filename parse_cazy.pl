b1a9a48da258211bcc1238ecf03c530d89ef2034
Package Admin <root@biocluster>
2021-07-06 15:10:44 -0700

    allow cmdline input for CAZy filename

--- parse_cazy.pl
+++ parse_cazy.pl
@@ -5,18 +5,18 @@
 #
 # Outputs a CSV file of id,description for import into sqlite
 #
+use strict;
+use warnings;
+my $infile = shift || "../CAZyDB.07312019.fam-activities.txt";
 
-open(IN, "../CAZyDB.07312019.fam-activities.txt");
+open(IN, $infile) || die "cannot open $infile: $!";
 while(<IN>) {
-        next if m/^#/;
-
-        chomp();
-
-        my($id,$desc) = split(/\t/);
-        $desc = $id if $desc eq "";
-
-        $desc =~ s/^\s+//;
-
-        print '"', $id, '","', $desc, '"', "\n";
+    next if /^\#/;
+    chomp;
+    my($id,$desc) = split(/\t/,$_);
+    $desc = $id if $desc eq "";
+    
+    $desc =~ s/^\s+//;
+    print join(",", map { sprintf('"%s"',$_) } ($id,$desc)),"\n";
 }
 close IN;
