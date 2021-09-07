97f2db88feabdd86b6c1c6a3f236a1b39d9eea1b
Jason Stajich <jasonstajich.phd@gmail.com>
2021-09-07 12:18:24 -0700

    missin folder name

--- make_dram_files.sh
+++ make_dram_files.sh
@@ -13,7 +13,7 @@ if [ ! -f dbcan_description.csv ]; then
     perl DRAM_hacks/parse_cazy.pl ../CAZyDB.07302020.fam-activities.txt  > dbcan_description.csv
 fi
 if [ ! -f vogdb_description.csv ]; then
-    perl parse_vog.pl ../vog_annotations_latest.tsv.gz > vogdb_description.csv
+    perl DRAM_hacks/parse_vog.pl ../vog_annotations_latest.tsv.gz > vogdb_description.csv
 fi
 if [ ! -f pfam_description.csv ]; then
     perl DRAM_hacks/parse_pfam.pl  ../Pfam-A.hmm.dat.gz > pfam_description.csv
